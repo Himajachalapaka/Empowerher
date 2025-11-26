6c4d6a396a639972f40eabcfc05d56c108bb3694
Himajachalapaka <himajachalapaka27@gmail.com>
2025-11-26 22:42:37 +0530

    Initial setup done

---
+++ Assign/q19.js
@@ -0,0 +1,11 @@
+let age = 10;
+function displayAge(){
+    console.log("Age is:",age)
+}
+function changeAge(){
+    age = 20;
+    console.log("Updated age is:",age)
+}
+displayAge();
+changeAge();
+displayAge();
